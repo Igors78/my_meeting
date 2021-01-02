c6c1e83e69b46e5ab673199a57f6ff17d57e65f0
Restyled.io <commits@restyled.io>
2021-01-02 18:34:14 +0000

    Restyled by rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,34 +12,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_162520) do
-
-  create_table "invites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "meeting_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["meeting_id"], name: "index_invites_on_meeting_id"
-    t.index ["user_id"], name: "index_invites_on_user_id"
+ActiveRecord::Schema.define(version: 20_210_102_162_520) do
+  create_table 'invites', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'meeting_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['meeting_id'], name: 'index_invites_on_meeting_id'
+    t.index ['user_id'], name: 'index_invites_on_user_id'
   end
 
-  create_table "meetings", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.datetime "date"
-    t.index ["user_id"], name: "index_meetings_on_user_id"
+  create_table 'meetings', force: :cascade do |t|
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'user_id', null: false
+    t.datetime 'date'
+    t.index ['user_id'], name: 'index_meetings_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_users_on_name", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['name'], name: 'index_users_on_name', unique: true
   end
 
-  add_foreign_key "invites", "meetings"
-  add_foreign_key "invites", "users"
-  add_foreign_key "meetings", "users"
+  add_foreign_key 'invites', 'meetings'
+  add_foreign_key 'invites', 'users'
+  add_foreign_key 'meetings', 'users'
 end
